21485248a0d3ed78534dc641bef5bf49e87efe24
Vuong Mai <maivanvuong@gmail.com>
2022-09-18 08:51:52 +0700

    refactor
    refactor

--- build.sh
+++ build.sh
@@ -14,10 +14,8 @@ if [ -z ${1+x} ]; then
 	exit 0
 elif [ "$1" = "clean" ]; then
 	rm -rf revanced-cache build.log build
-	reset_template
 	exit 0
 elif [ "$1" = "reset-template" ]; then
-	reset_template
 	exit 0
 elif [ "$1" = "build" ]; then
 	:
@@ -31,8 +29,7 @@ log "$(date +'%Y-%m-%d')\n"
 mkdir -p "$BUILD_DIR" "$TEMP_DIR"
 
 if [ "$UPDATE_PREBUILTS" = true ]; then get_prebuilts; else set_prebuilts; fi
-reset_template
-get_cmpr
+
 
 if [ "$BUILD_YT" = true ]; then build_yt; fi
 if [ "$BUILD_MUSIC_ARM64_V8A" = true ]; then build_music $ARM64_V8A; fi
@@ -41,15 +38,6 @@ if [ "$BUILD_TWITTER" = true ]; then build_twitter; fi
 if [ "$BUILD_REDDIT" = true ]; then build_reddit; fi
 if [ "$BUILD_WARN_WETTER" = true ]; then build_warn_wetter; fi
 if [ "$BUILD_TIKTOK" = true ]; then build_tiktok; fi
-if [ "$BUILD_MINDETACH_MODULE" = true ]; then
-	echo "Building mindetach module"
-	cd mindetach-magisk/mindetach/
-	: >detach.txt
-	echo "com.google.android.youtube" >>detach.txt
-	echo "com.google.android.apps.youtube.music" >>detach.txt
-	zip -r ../../build/mindetach.zip .
-	cd ../../
-fi
 
-reset_template
+
 echo "Done"
